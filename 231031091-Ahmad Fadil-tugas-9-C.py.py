d98d5762e1762bac37b8ca2673a1a616f461ad93
afdil445 <144764223+afdil445@users.noreply.github.com>
2023-12-22 19:21:18 +0700

    Add files via upload

---
+++ 231031091-Ahmad Fadil-tugas-9-C.py.py
@@ -0,0 +1,30 @@
+vd = {"nama" : "MUHAMMAD RAFIF SADJID",
+      "kelas" : "SI23-C",
+      "nim" : "231031073",
+      "asal" : "MAMUJU",
+      "tanggal" : "29-APRIL-2005",
+      "alamat" : "Jalan kelapa gading",
+      "hobi" : "Work Out",
+      "tinggi" : "162 cm",
+      "makanan favorit" : "nasi uduk",
+      "minuman favorit" : "susu",
+      "lagu favorit" : "Battle scars",
+      "warna favorit" : "hitam",
+      "mimpi" : "Touring naik motor keliling indonesia",
+      "film favorit" : "The 3 idiots"
+      }
+print("Nama Lengkap  :",vd["nama"])
+print("Kelas         :",vd["kelas"])
+print("NIM           :",vd["nim"])
+print("Tmpt tgl lahir:",vd["asal"],vd["tanggal"])
+print("Alamat        :",vd["alamat"])
+print("Hobi          :",vd["hobi"])
+print("Tinggi        :",vd["tinggi"])
+print("makanan favorit        :",vd["makanan favorit"])
+print("minuman favorit        :",vd["minuman favorit"])
+print("lagu favorit        :",vd["lagu favorit"])
+print("warna favorit        :",vd["warna favorit"])
+print("mimpi        :",vd["mimpi"])
+print("film favorit        :",vd["film favorit"])
+
+print("\nTipe data var vd : ",type(vd))
